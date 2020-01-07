f6f32c50d151036045da03ee034bb4ed6b10ebe5
Andrew C <andrewc.london2019a@gmail.com>
2020-01-07 13:46:29 +0000

    Transaction, Account, CreditAccount Tested

--- src/test/java/com/qa/bankingcore/CreditAccountTest.java
+++ src/test/java/com/qa/bankingcore/CreditAccountTest.java
@@ -22,4 +22,20 @@ class CreditAccountTest {
         assertEquals(expectedResult,actualResult);
     }
 
+    @Test
+    public void does_balance_match_after_debit_on_credit_account() {
+        //arrange
+        double openingBalance = 156.96;
+        double amountToDebit = 54.67;
+        double expectedResult = openingBalance - amountToDebit;
+
+        //act
+        Account acc = new CreditAccount(1,"Andrew",openingBalance,InterestRate.HOME_LOAN);
+        acc.debit(amountToDebit);
+        double actualResult = acc.currentBalance();
+
+        //assert
+        assertEquals(expectedResult,actualResult);
+    }
+
 }
